65a43f8cfde52d7efaed32926a6d2e9774063ad9
EnriqueYt2017 <137914465+EnriqueYt2017@users.noreply.github.com>
2023-06-27 12:41:26 -0400

    Segunda version

--- funciones.py
+++ funciones.py
@@ -5,9 +5,34 @@ from colorama import Fore,Style,Back ,init
 init()
 init(autoreset=True)
 
-depas=np.empty([10,4],object)
+depas=np.empty([2,10,4],object)
 
-print(depas)
+def dibujaredificio():
+    print(F'{Style.BRIGHT}    A   B   C   D ')
+    for x in range(10): #filas
+        if 10-x<10:
+            print(f'0{10-x}',end='|')
+        else:
+            print(10-x,end='|')
+        for y in range(4): #Columnas
+            if depas[0,x,y]==None:
+                print(f'🟩',end='|')
+            else:
+                print(f'🟥',end='|')
+        print(' ')
 
+def limpiarpantalla():
+    pass
 
+def printr(texto):
+    pass
+
+def comprardepa(piso,columna):
+    pass
+
+def pagar(costo,pago):
+    pass
+
+def buscardueno(nombre):
+    pass
 
